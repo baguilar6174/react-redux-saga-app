fdf1c35ced7a7ba7a494ac04934357dfffaee448
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2022-08-08 12:06:19 -0500

    feat: Add initial UI to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+	Button,
+	Container,
+	Form,
+	Grid,
+	Header,
+	Icon,
+	Segment,
+	Statistic,
+} from 'semantic-ui-react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<Container>
+			<Header as='h1'>Budget</Header>
+			<Statistic size='small'>
+				<Statistic.Label>Your Balance:</Statistic.Label>
+				<Statistic.Value>2500.53</Statistic.Value>
+			</Statistic>
+			<Segment textAlign='center'>
+				<Grid columns={2} divided>
+					<Grid.Row>
+						<Grid.Column>
+							<Statistic size='tiny' color='green'>
+								<Statistic.Label style={{ textAlign: 'left' }}>
+									Income:
+								</Statistic.Label>
+								<Statistic.Value>1502</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+						<Grid.Column>
+							<Statistic size='tiny' color='red'>
+								<Statistic.Label style={{ textAlign: 'left' }}>
+									Expenses:
+								</Statistic.Label>
+								<Statistic.Value>850</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
+			<Header as={'h3'}>History</Header>
+			<Segment color='red'>
+				<Grid columns={3} textAlign='right'>
+					<Grid.Row>
+						<Grid.Column width={10} textAlign='left'>
+							Someting
+						</Grid.Column>
+						<Grid.Column width={3} textAlign='right '>
+							$10.00
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Icon name='edit' bordered />
+							<Icon name='trash' bordered />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
+			<Segment color='red'>
+				<Grid columns={3} textAlign='right'>
+					<Grid.Row>
+						<Grid.Column width={10} textAlign='left'>
+							Someting
+						</Grid.Column>
+						<Grid.Column width={3} textAlign='right '>
+							$10.00
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Icon name='edit' bordered />
+							<Icon name='trash' bordered />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+			<Header as={'h3'}>Add new transation</Header>
+			<Form unstackable>
+				<Form.Group>
+					<Form.Input
+						icon={'tags'}
+						width={12}
+						label='Description'
+						placeholder='New shinny thing'
+					/>
+					<Form.Input
+						width={4}
+						label='Value'
+						placeholder='1000'
+						icon={'dollar'}
+						iconPosition='left'
+					/>
+				</Form.Group>
+				<Button.Group style={{ marginTop: 20 }}>
+					<Button>Cancel</Button>
+					<Button.Or />
+					<Button primary>Ok</Button>
+				</Button.Group>
+			</Form>
+		</Container>
+	);
 }
 
-export default App
+export default App;
